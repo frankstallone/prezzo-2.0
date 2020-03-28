5dadf3843bd1f2216d29f3652ab5c8f86ec2de8f
Frank Stallone <frank@stallone.dev>
2020-03-28 07:36:23 -0400

    wip: 🚧 Porting old theme to new, much more modern theme

---
+++ wp-content/themes/prezzo/functions.php
@@ -0,0 +1,53 @@
+<?php
+
+// Main enqueue scripts and front page specific enqueued items
+function prezzo_theme_enqueue_scripts()
+{
+    // Divi's styles
+    wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
+
+    // JS for site in footer
+    wp_enqueue_script('scripts', get_stylesheet_directory_uri() . '/js/footer-scripts.js', array('jquery'), '1.1', true);
+
+    // 🚨 Front page only below this line
+    if (!is_front_page()) return;
+
+    // Home page specific JS
+    wp_enqueue_script('home-scripts', get_stylesheet_directory_uri() . '/js/home-scripts.js', array('jquery'), '1.1', true);
+    // Use this echo example as for <script> or <meta> needed for home page
+    // echo '<meta name="google-site-verification" content="xxx" />';
+}
+
+// Execute
+add_action('wp_enqueue_scripts', 'prezzo_theme_enqueue_scripts');
+
+// Footer specific scripts
+function footer_scripts()
+{ ?>
+    <!-- Font-Awesome Pro kit for Prezzo.it -->
+    <script src="https://kit.fontawesome.com/24e03df92e.js" crossorigin="anonymous"></script>
+<?php
+}
+
+// Execute
+add_action('wp_footer', 'footer_scripts');
+
+// Remove WP Emoji
+remove_action('wp_head', 'print_emoji_detection_script', 7);
+remove_action('wp_print_styles', 'print_emoji_styles');
+remove_action('admin_print_scripts', 'print_emoji_detection_script');
+remove_action('admin_print_styles', 'print_emoji_styles');
+
+
+// Remove query strings!
+function _remove_script_version($src)
+{
+    $parts = explode('?ver', $src);
+    return $parts[0];
+}
+
+// Removing query strings -- more @ https://www.keycdn.com/support/remove-query-strings-from-static-resources
+add_filter('script_loader_src', '_remove_script_version', 15, 1);
+add_filter('style_loader_src', '_remove_script_version', 15, 1);
+
+?>
